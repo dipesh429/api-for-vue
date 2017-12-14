e6b61d02c0b05ab6c8ea5849a133b99688c8f137
Dipesh <kcdipesh429@gmail.com>
2017-12-14 16:44:45 +0545

    cors problem solved again

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,10 +15,22 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-                  ->header('Access-Control-Allow-Origin','https://dipes.herokuapp.com')
-                  ->header('Access-Control-Allow-Methods','*')
-                  ->header('Access-Control-Allow-Headers','*');
+        // return $next($request)
+
+
+        //           ->header('Access-Control-Allow-Origin','https://dipes.herokuapp.com')
+        //           ->header('Access-Control-Allow-Methods','*')
+        //           ->header('Access-Control-Allow-Headers','*');
+
+
+       $response = $next($request);
+       $response->header('Access-Control-Allow-Origin','https://dipes.herokuapp.com');
+       $response->header('Access-Control-Allow-Methods','*');
+       $response->header('Access-Control-Allow-Headers','*');
+       return $response;
+
+
+
 
               
     }
