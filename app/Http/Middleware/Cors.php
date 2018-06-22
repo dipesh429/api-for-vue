875305958d2b1e0b89596fa3113382716901dbc3
Dipesh <kcdipesh429@gmail.com>
2018-06-22 16:53:42 +0545

    adf

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,18 +15,16 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
+        // return $next($request)
 
 
-                  // ->header('Access-Control-Allow-Origin','https://dipes.herokuapp.com')
-                  ->header('Access-Control-Allow-Origin','*')
-                  ->header('Access-Control-Allow-Methods','*')
-                  ->header('Access-Control-Allow-Headers','*');
 
-              
-    }
+       $response = $next($request);
+       $response->header('Access-Control-Allow-Origin','https://dipes.herokuapp.com');
+       $response->header('Access-Control-Allow-Methods','*');
+       $response->header('Access-Control-Allow-Headers','*');
+       return $response;
 }
-
 
 
 
